22257c4ba9a1b5f2a33d1a14d562a0622de6b23b
Umer Faizyab <umerfaizyab123@gmail.com>
2022-04-02 20:03:13 +0500

    work complete

---
+++ new.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <conio.h>
+int main(){
+	
+//----------------------------------------Experiment 4---------------------------------------	
+	
+	
+//	char name[30],fname[30],address[30],qual[30],mail[30];
+//	int phone,age;
+//	printf("Enter Your Name :");
+//	fgets(name,30,stdin);
+//	printf("Enter Your Father's Name :");
+//	fgets(fname,30,stdin);
+//	printf("Enter Your Address  :");
+//	fgets(address,30,stdin);
+//	printf("Enter Your QUalification :");
+//	fgets(qual,30,stdin);
+//	printf("Enter Your email :");
+//	fgets(mail,30,stdin);
+//	printf("Enter Your Phone :");
+//	scanf("%d",&phone);
+//	printf("Enter Your Age :");
+//	scanf("%d",&age);
+//
+//	printf("%s%s","Name: ",name);
+//	printf("%s%s","Father Name: ",fname);
+//	printf("%s%s", "Address: ",address);
+//	printf("%s%s", "Qualification: ",qual);
+//	printf("%s%s", "Email :",mail);
+//	printf("%s%d\n", "Phone: +",phone);
+//	printf("%s%d", "Age : ", age);
+
+//----------------------------------------Experiment 4---------------------------------------
+
+
+//----------------------------------------Experiment 3---------------------------------------
+
+
+//	char name[] = "Umer Faizyab Khan";
+//	char fname[] = "Obaidullah Khan";
+//	char address[] = "Address: Flat #123, Sohni Golf view Appt, Karachi";
+//	char qual[] = "Intermidiate"; 
+//	char mail[] = "umerfaizyab123@gmail.com";
+//	int phone = 921234567;
+//	int age = 20;
+//	printf("%s%s","Name: ",name);
+//	printf("\n%s%s","Father Name: ",fname);
+//	printf("\n%s%s", "Address: ",address);
+//	printf("\n%s%ld", "Phone: +",phone);
+//	printf("\n%s%s", "Qualification: ",qual);
+//	printf("\n%s%d", "Age : ", age);
+//	printf("\n%s%s", "Email :",mail);
+
+//----------------------------------------Experiment 3---------------------------------------
+
+
+//----------------------------------------Experiment 2---------------------------------------
+
+
+//	printf("%s","Name: Umer Faizyab Khan");
+//	printf("\n%s","Father Name: Obaidullah Khan");
+//	printf("\n%s", "Address: Flat #123, Sohni Golf view Appt, Karachi");
+//	printf("\n%s%ld%ld", "Phone: +",92123,45678);
+//	printf("\n%s", "Qualification: Intermidiate");
+//	printf("\n%s%d", "Age : ", 20);
+//	getch();	
+
+//----------------------------------------Experiment 2---------------------------------------
+
+//}
